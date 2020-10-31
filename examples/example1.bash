1378f43ddfe83a345d7dbe689c8b2a9e13c2c6d2
Igor Lesik <ilesik@tachyum.com>
2020-10-31 08:39:34 -0700

    create docs dir

--- examples/example1.bash
+++ examples/example1.bash
@@ -4,4 +4,4 @@ HOME=$(dirname ${BASH_SOURCE[0]})/..
 VERIDOC=$HOME/src/veridoc/veridoc.py
 VLOGFILE=$HOME/examples/rtl/lib/gates/generic/OAI22.sv
 
-echo "python3 $VERIDOC -s $VLOGFILE > $HOME/examples/example1.html"
+echo "python3 $VERIDOC -s $VLOGFILE > $HOME/docs/example1.html"
